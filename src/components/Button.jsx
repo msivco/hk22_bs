052c88b90d742b7ec2b524c6f36645b9c77d7514
Gilles Tanson <gillestanson@googlemail.com>
2022-04-23 14:41:35 +0200

    add components folder

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { Button } from "@mui/material";
+
+function CustomButton() {
+	const [state, setState] = useState("")
+
+	return (
+		<Button>{Children}</Button>
+	)
+}
+
+export default CustomButton;
